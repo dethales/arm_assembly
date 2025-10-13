b63af9e0de4a4194b32514bec8369fbd19cc6615
dethales <thaleslayber@gmail.com>
2025-10-13 12:59:08 -0300

    Adicionado arquivo template para começar cada lab

---
+++ template.s
@@ -0,0 +1,12 @@
+.equ PRINT_CHAR_STDOUT,     0x00
+.equ PRINT_STRING_STDOUT,   0x02
+.equ PRINT_INT,             0x6B
+.equ STDOUT,                0x01
+.equ EXIT,                  0x11
+
+.text
+_start:
+
+.data
+
+.end
